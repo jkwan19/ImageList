e16192d9b8633bf1f51a71b68bddec75fbcf1d0c
Justin Kwan <jkwan19@gmail.com>
2021-05-18 22:03:19 -0700

    Handle search text

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: '10px' }}>
-      <SearchBar/>
+class App extends React.Component {
+
+  onSearchSubmit(term){
+    console.log(term)
+  }
+
+  render() {
+
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+      <SearchBar handleSubmit={this.onSearchSubmit}/>
     </div>
-  )
+    )
+  }
 }
 
 export default App;
